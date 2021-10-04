469ea1cccde62bc65d28157ec326bf4f20d4e63e
CodingNoJam <hanwoodww@gmail.com>
2021-10-05 02:13:33 +0900

    fix: 3자리의 랜덤한 숫자 생성기능 수정
    - assNumber() 메서드 추가
    - Computer 클래스 내부 컬렉션 수정 Set -> List
    - getNumbers() 리턴타입 수정 Set -> List
    
    기존 코드는 랜덤숫자가 아니라 정렬된 3자리의 숫자를 반환하므로 수정

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,28 +1,37 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import nextstep.utils.Randoms;
-import java.util.HashSet;
-import java.util.Set;
 
 public class Computer {
-    private Set<Integer> numbers = new HashSet<>();
+	private List<Integer> numbers = new ArrayList<>();
 
-    private Computer(){
-    }
+	private Computer() {
+	}
 
-    public static Computer createAndInit() {
-        Computer computer = new Computer();
-        computer.createRandomNumbers();
-        return computer;
-    }
+	public static Computer createAndInit() {
+		Computer computer = new Computer();
+		computer.createRandomNumbers();
+		return computer;
+	}
 
-    private void createRandomNumbers() {
-        while (numbers.size() < 3) {
-            numbers.add(Randoms.pickNumberInRange(1, 9));
-        }
-    }
+	private void createRandomNumbers() {
+		while (numbers.size() < 3) {
+			addNumber();
+		}
+	}
 
-    public Set<Integer> getNumbers() {
-        return numbers;
-    }
+	private void addNumber() {
+		int number = Randoms.pickNumberInRange(1, 9);
+		if (numbers.contains(number)) {
+			return;
+		}
+		numbers.add(number);
+	}
+
+	public List<Integer> getNumbers() {
+		return numbers;
+	}
 }
