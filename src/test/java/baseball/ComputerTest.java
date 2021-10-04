8e3b1092c92415ceaf93c168e3112017a6ecc9fa
CodingNoJam <hanwoodww@gmail.com>
2021-10-05 02:59:56 +0900

    test: 3자리의 랜덤한 숫자 생성기능 테스트 수정
    - Computer 클래스 수정으로 인해 테스트 코드 수정
    - 랜덤숫자 관련 테스트 코드 요소별로 분할해서 작성

--- src/test/java/baseball/ComputerTest.java
+++ src/test/java/baseball/ComputerTest.java
@@ -1,10 +1,15 @@
 package baseball;
 
 import static org.assertj.core.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+
 import org.junit.jupiter.api.Test;
 
-class ComputerTest {
+public class ComputerTest {
 
 	@Test
 	void 컴퓨터객체_생성_테스트() {
@@ -13,13 +18,29 @@ class ComputerTest {
 	}
 
 	@Test
-	void 랜덤숫자_생성_테스트() {
+	void 랜덤숫자_자릿수_테스트() {
 		Computer computer = Computer.createAndInit();
-		Set<Integer> numbers = computer.getNumbers();
+		List<Integer> numbers = computer.getNumbers();
 		assertThat(numbers).hasSize(3);
+	}
+
+	@Test
+	void 랜덤숫자_범위_테스트() {
+		Computer computer = Computer.createAndInit();
+		List<Integer> numbers = computer.getNumbers();
 		for (Integer number : numbers) {
-			System.out.println(number);
 			assertThat(number).isGreaterThanOrEqualTo(1).isLessThanOrEqualTo(9);
 		}
+	}
+
+	@Test
+	void 랜덤숫자_중복여부_테스트() {
+		Computer computer = Computer.createAndInit();
+		List<Integer> numbers = computer.getNumbers();
+		Set<Integer> checkSet = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+		for (Integer number : numbers) {
+			checkSet.remove(number);
+		}
+		assertThat(numbers.size()).isEqualTo(9 - checkSet.size());
 	}
 }
