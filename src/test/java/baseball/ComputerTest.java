9945e3b030479b4e5ac28017403562ced6b58c0d
CodingNoJam <hanwoodww@gmail.com>
2021-10-05 12:12:01 +0900

    test: 플레이어로부터 입력받은 값에 대한 검증 및 에러처리 테스트
    - 입력값에 대한 길이와 타입 검증 테스트 코드 추가
    - 입력값에 대한 중복 검증 테스트 코드 추가
    - beforeEach() : 테스트 시작 전 Computer 객체 생성
    - 기존 테스트코드에서 직접 Computer 객체 생성하는 코드 삭제

--- src/test/java/baseball/ComputerTest.java
+++ src/test/java/baseball/ComputerTest.java
@@ -7,26 +7,32 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class ComputerTest {
+	private Computer computer;
+
+	@BeforeEach
+	void beforeEach() {
+		computer = Computer.createAndInit();
+	}
 
 	@Test
 	void 컴퓨터객체_생성_테스트() {
-		Computer computer = Computer.createAndInit();
 		assertThat(computer).isNotNull();
 	}
 
 	@Test
 	void 랜덤숫자_자릿수_테스트() {
-		Computer computer = Computer.createAndInit();
 		List<Integer> numbers = computer.getNumbers();
 		assertThat(numbers).hasSize(3);
 	}
 
 	@Test
 	void 랜덤숫자_범위_테스트() {
-		Computer computer = Computer.createAndInit();
 		List<Integer> numbers = computer.getNumbers();
 		for (Integer number : numbers) {
 			assertThat(number).isGreaterThanOrEqualTo(1).isLessThanOrEqualTo(9);
@@ -35,12 +41,27 @@ public class ComputerTest {
 
 	@Test
 	void 랜덤숫자_중복여부_테스트() {
-		Computer computer = Computer.createAndInit();
 		List<Integer> numbers = computer.getNumbers();
 		Set<Integer> checkSet = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
 		for (Integer number : numbers) {
 			checkSet.remove(number);
 		}
 		assertThat(numbers.size()).isEqualTo(9 - checkSet.size());
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"1234", "우아한테크코스", "1", "39", "car", "000"})
+	void 플레이어_입력값_길이_타입_검증_테스트(String input) {
+		String[] answer = computer.createAnswer(input);
+		assertThat(answer[0]).isEqualTo("3");
+		assertThat(answer[1]).isEqualTo("[ERROR] 3자리 숫자만 입력가능하며, 숫자는 1~9사이의 값이여야 합니다.");
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"999", "121", "939", "422", "882", "111"})
+	void 플레이어_입력값_중복_검증_테스트(String input) {
+		String[] answer = computer.createAnswer(input);
+		assertThat(answer[0]).isEqualTo("3");
+		assertThat(answer[1]).isEqualTo("[ERROR] 중복된 숫자는 입력할 수 없습니다.");
 	}
 }
