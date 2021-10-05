90ccedfee172edfb4a866094a38e893c1159aaa1
CodingNoJam <hanwoodww@gmail.com>
2021-10-05 14:59:39 +0900

    feat: 게임이 끝난 경우 재 시작 여부 확인을 위한 문구 출력기능 구현
    - enterRestartOrEnd() : 재 시작 여부 확인을 위한 문구 출력 후 입력 받기

--- src/main/java/baseball/Printer.java
+++ src/main/java/baseball/Printer.java
@@ -11,4 +11,9 @@ public class Printer {
 	public static void printMessage(String message) {
 		System.out.println(message);
 	}
+
+	public static String enterRestartOrEnd() {
+		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+		return Console.readLine();
+	}
 }
