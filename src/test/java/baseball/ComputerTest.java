838ff4e3439b31011650769fcf2ad2e4222d041d
CodingNoJam <hanwoodww@gmail.com>
2021-10-05 16:31:56 +0900

    test: ComputerTest 클래스 테스트코드 수정
    - Computer 클래스 리팩토링으로 인해 테스트 코드 수정
    - 데이터 검증관련 테스트 코드 제거
    - 응답메세지 관련 코드 값 테스트에서 제거

--- src/test/java/baseball/ComputerTest.java
+++ src/test/java/baseball/ComputerTest.java
@@ -9,8 +9,6 @@ import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 public class ComputerTest {
 	private Computer computer;
@@ -52,30 +50,13 @@ public class ComputerTest {
 		assertThat(numbers.size()).isEqualTo(9 - checkSet.size());
 	}
 
-	@ParameterizedTest
-	@ValueSource(strings = {"1234", "우아한테크코스", "1", "39", "car", "000"})
-	void 플레이어_입력값_길이_타입_검증_테스트(String input) {
-		String[] answer = computer.createAnswer(input);
-		assertThat(answer[0]).isEqualTo("30");
-		assertThat(answer[1]).isEqualTo("[ERROR] 3자리 숫자만 입력가능하며, 숫자는 1~9사이의 값이여야 합니다.");
-	}
-
-	@ParameterizedTest
-	@ValueSource(strings = {"999", "121", "939", "422", "882", "111"})
-	void 플레이어_입력값_중복_검증_테스트(String input) {
-		String[] answer = computer.createAnswer(input);
-		assertThat(answer[0]).isEqualTo("31");
-		assertThat(answer[1]).isEqualTo("[ERROR] 중복된 숫자는 입력할 수 없습니다.");
-	}
-
 	@Test
 	void 플레이어_입력값_응답_정답처리_테스트() {
 		for (int number : computer.getNumbers()) {
 			stringBuilder.append(String.valueOf(number));
 		}
-		String[] answer = computer.createAnswer(stringBuilder.toString());
-		assertThat(answer[0]).isEqualTo("50");
-		assertThat(answer[1]).isEqualTo("3개의 숫자를 모두 맞히셨습니다! 게임 끝");
+		String answer = computer.createHint(stringBuilder.toString());
+		assertThat(answer).isEqualTo("3스트라이크 \n3개의 숫자를 모두 맞히셨습니다! 게임 끝");
 	}
 
 	@Test
@@ -86,9 +67,8 @@ public class ComputerTest {
 		for (Integer number : checkSet) {
 			stringBuilder.append(number);
 		}
-		String[] answer = computer.createAnswer(stringBuilder.substring(0, 3));
-		assertThat(answer[0]).isEqualTo("40");
-		assertThat(answer[1]).isEqualTo("낫싱");
+		String answer = computer.createHint(stringBuilder.substring(0, 3));
+		assertThat(answer).isEqualTo("낫싱");
 	}
 
 	@Test
@@ -100,9 +80,8 @@ public class ComputerTest {
 		for (Integer number : checkSet) {
 			stringBuilder.append(number);
 		}
-		String[] answer = computer.createAnswer(stringBuilder.substring(0,3));
-		assertThat(answer[0]).isEqualTo("41");
-		assertThat(answer[1]).isEqualTo("1스트라이크");
+		String answer = computer.createHint(stringBuilder.substring(0,3));
+		assertThat(answer).isEqualTo("1스트라이크");
 	}
 
 	@Test
@@ -112,9 +91,8 @@ public class ComputerTest {
 		stringBuilder.append(testList.get(2));
 		stringBuilder.append(testList.get(0));
 
-		String[] answer = computer.createAnswer(stringBuilder.toString());
-		assertThat(answer[0]).isEqualTo("42");
-		assertThat(answer[1]).isEqualTo("3볼");
+		String answer = computer.createHint(stringBuilder.toString());
+		assertThat(answer).isEqualTo("3볼");
 	}
 
 	@Test
@@ -124,8 +102,7 @@ public class ComputerTest {
 		stringBuilder.append(testList.get(2));
 		stringBuilder.append(testList.get(1));
 
-		String[] answer = computer.createAnswer(stringBuilder.toString());
-		assertThat(answer[0]).isEqualTo("43");
-		assertThat(answer[1]).isEqualTo("1스트라이크 2볼");
+		String answer = computer.createHint(stringBuilder.toString());
+		assertThat(answer).isEqualTo("1스트라이크 2볼");
 	}
 }
