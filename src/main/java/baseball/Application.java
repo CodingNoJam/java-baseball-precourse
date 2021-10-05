ad46cfad5ca37102ba49367714d70a7a0a03e273
CodingNoJam <hanwoodww@gmail.com>
2021-10-05 17:15:35 +0900

    feat: Application 클래스에 게임 구현
    - main() 메서드 내부에 게임로직 구현
    - GameController 클래스를 통해 게임 시작, 종료 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,14 @@
 package baseball;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO 숫자 야구 게임 구현
-    }
+	public static void main(String[] args) {
+		// TODO 숫자 야구 게임 구현
+		boolean gameStart = true;
+		while (gameStart) {
+			GameController gameController = new GameController();
+			gameController.run();
+			gameStart = gameController.restartOrEnd();
+		}
+	}
 }
+
