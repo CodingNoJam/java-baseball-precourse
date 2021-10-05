59b397c24dacbc5f18c0eccd9e63a9ae5f5d1081
CodingNoJam <hanwoodww@gmail.com>
2021-10-05 11:46:18 +0900

    feat: 플레이어로부터 입력받은 값에 대한 검증 및 에러처리
    - Computer 클래스 내부에 검증 메서드 추가
    - verifyDataLengthAndDataType() : 입력받은 값의 길이와 타입 검증
    - verifyDuplicateData() : 입력받은 값의 중복여부 검증

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,7 +1,9 @@
 package baseball;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import nextstep.utils.Randoms;
 
@@ -33,5 +35,38 @@ public class Computer {
 
 	public List<Integer> getNumbers() {
 		return numbers;
+	}
+
+	public String[] createAnswer(String data) {
+		if (!verifyDataLengthAndDataType(data)) {
+			return new String[] {"3", "[ERROR] 3자리의 숫자만 입력가능합니다."};
+		}
+		if (!verifyDuplicateData(data)) {
+			return new String[] {"3", "[ERROR] 중복된 숫자는 입력할 수 없습니다."};
+		}
+		return checkNumber(data);
+	}
+
+	private boolean verifyDataLengthAndDataType(String data) {
+		if (data.matches("^[1-9]{3}$")) {
+			return true;
+		}
+		return false;
+	}
+
+	private boolean verifyDuplicateData(String data) {
+		Set<Character> duplicateCheckSet = new HashSet<>();
+		for (int i = 0; i < data.length(); i++) {
+			duplicateCheckSet.add(data.charAt(i));
+		}
+		if (duplicateCheckSet.size() == 3) {
+			return true;
+		}
+		return false;
+	}
+
+	private String[] checkNumber(String data) {
+
+		return new String[] {};
 	}
 }
