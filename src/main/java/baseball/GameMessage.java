bdf0a0026dc2030ebac3086767106c406c97af16
CodingNoJam <hanwoodww@gmail.com>
2021-10-05 17:46:56 +0900

    refactor: GameMessage 상수 추가

--- src/main/java/baseball/GameMessage.java
+++ src/main/java/baseball/GameMessage.java
@@ -7,6 +7,7 @@ public class GameMessage {
 	public static final String ANSWER_BALL = "볼";
 	public static final String ANSWER_STRIKE = "스트라이크";
 	public static final String ANSWER_THREE_STRIKE = "스트라이크 \n3개의 숫자를 모두 맞히셨습니다! 게임 끝";
+	public static final String ANSWER_END = "끝";
 	public static final String ERROR_DATA_LENGTH_DATA_TYPE = "[ERROR] 3자리 숫자만 입력가능하며, 숫자는 1~9사이의 값이여야 합니다.";
 	public static final String ERROR_DUPLICATE_DATA = "[ERROR] 중복된 숫자는 입력할 수 없습니다.";
 	public static final String ERROR_RESTART_OR_END_DATA_WRONG_VALUE = "[ERROR] 1 또는 2만 입력 가능합니다.";
