55b6ec6ec2bf30f1f33320c6ccb28e0dc3427304
CodingNoJam <hanwoodww@gmail.com>
2021-10-05 10:15:17 +0900

    feat: 플레이어에게 3자리 수 입력문구 출력 및 입력받기 기능 구현
    - 화면에 문구 출력을 담당하는 Printer class 추가
    - enterNumbers() : 숫자 입력받기

---
+++ src/main/java/baseball/Printer.java
@@ -0,0 +1,10 @@
+package baseball;
+
+import nextstep.utils.Console;
+
+public class Printer {
+	public static String enterNumbers() {
+		System.out.print("숫자를 입력해주세요 : ");
+		return Console.readLine();
+	}
+}
