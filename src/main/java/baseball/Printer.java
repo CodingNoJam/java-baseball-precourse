52d5739e7bd857d3ae0836ad50217db3c518941c
CodingNoJam <hanwoodww@gmail.com>
2021-10-05 14:39:46 +0900

    feat: Printer 클래스에 입력받은 문자열 출력하는 기능 추가

--- src/main/java/baseball/Printer.java
+++ src/main/java/baseball/Printer.java
@@ -7,4 +7,8 @@ public class Printer {
 		System.out.print("숫자를 입력해주세요 : ");
 		return Console.readLine();
 	}
+
+	public static void printMessage(String message) {
+		System.out.println(message);
+	}
 }
