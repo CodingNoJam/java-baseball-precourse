35ed6e51b9ebef6a0a29e013fdf015ac173dc422
CodingNoJam <hanwoodww@gmail.com>
2021-10-05 16:40:25 +0900

    refactor: GameController 클래스 리팩토링
    - Computer 클래스의 verifyXxx() 메서드 추가
    - Computer 클래스의 createAnswer() 메서드 추가
    - 게임진행 과정에서 데이터 검증은 GameController 클래스에서 처리하도록 리팩토링

--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -1,5 +1,8 @@
 package baseball;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class GameController {
 	private final Computer computer;
 
@@ -11,9 +14,58 @@ public class GameController {
 		boolean running = true;
 		while (running) {
 			String data = Printer.enterNumbers();
-			String[] answer = computer.createAnswer(data);
-			Printer.printMessage(answer[1]);
-			running = !answer[0].startsWith("5");
+			String answer = createAnswer(data);
+			Printer.printMessage(answer);
+			running = !answer.endsWith("끝");
 		}
+	}
+
+	private String createAnswer(String data) {
+		if (!verifyDataLengthAndDataType(data)) {
+			return "[ERROR] 3자리 숫자만 입력가능하며, 숫자는 1~9사이의 값이여야 합니다.";
+		}
+		if (!verifyDuplicateData(data)) {
+			return "[ERROR] 중복된 숫자는 입력할 수 없습니다.";
+		}
+		return computer.createHint(data);
+	}
+
+	private boolean verifyDataLengthAndDataType(String data) {
+		if (data.matches("^[1-9]{3}$")) {
+			return true;
+		}
+		return false;
+	}
+
+	private boolean verifyDuplicateData(String data) {
+		Set<Character> duplicateCheckSet = new HashSet<>();
+		for (int i = 0; i < data.length(); i++) {
+			duplicateCheckSet.add(data.charAt(i));
+		}
+		if (duplicateCheckSet.size() == 3) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean restartOrEnd() {
+		boolean reQuestions = true;
+		String data = "";
+		while (reQuestions) {
+			data = Printer.enterRestartOrEnd();
+			reQuestions = verifyDataWrongValue(data);
+		}
+		if (data.equals("1")) {
+			return true;
+		}
+		return false;
+	}
+
+	private boolean verifyDataWrongValue(String data) {
+		if (data.equals("1") || data.equals("2")) {
+			return false;
+		}
+		Printer.printMessage("[ERROR] 1 또는 2만 입력 가능합니다.");
+		return true;
 	}
 }
